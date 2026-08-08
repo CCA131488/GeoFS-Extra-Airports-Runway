3c85edc8f9130edf7c9adb16645bd52feed9d203
CES2731 <1830524785@qq.com>
2026-08-08 18:20:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2026-04-21
 // @description  Extra Runways
-// @author       CES2731 & Deepseek
+// @author       CES2731 & Deepseek & CP8888
 // @match        https://geo-fs.com/geofs.php*
 // @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
